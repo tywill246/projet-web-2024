db2c6a6aa12613e990977428d6c10d758a4f46dd
William Laflamme <6267802@etu.cegep-lanaudiere.qc.ca>
2024-03-14 15:32:35 -0400

    commentaires du html et css

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('menu-toggle')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
